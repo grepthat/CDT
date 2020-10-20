2e4608cbda0cfb781e1ab60ca8d9a5cdd339c9a7
artem-ogre <artem.ogre@gmail.com>
2020-10-20 13:26:48 +0200

    Add doxygen

---
+++ doxygen/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['findduplicates',['FindDuplicates',['../namespaceCDT.html#a24f9bf727d1254a1c71c9f74c4f4a800',1,'CDT::FindDuplicates(TVertexIter first, TVertexIter last, TGetVertexCoordX getX, TGetVertexCoordY getY)'],['../namespaceCDT.html#af2a13e7cc765f757db88a398a5221bc4',1,'CDT::FindDuplicates(TVertexIter first, TVertexIter last, TGetVertexCoordX getX, TGetVertexCoordY getY)']]],
+  ['findingclosestpoint',['FindingClosestPoint',['../structCDT_1_1FindingClosestPoint.html',1,'CDT']]],
+  ['fixededges',['fixedEdges',['../classCDT_1_1Triangulation.html#afc2251094e2b78fdc75b2bbed00598a1',1,'CDT::Triangulation']]]
+];
---
+++ doxygen/search/functions_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['insertedges',['insertEdges',['../classCDT_1_1Triangulation.html#a62ed447532ee7652c3300bd535fbbfb0',1,'CDT::Triangulation::insertEdges(TEdgeIter first, TEdgeIter last, TGetEdgeVertexStart getStart, TGetEdgeVertexEnd getEnd)'],['../classCDT_1_1Triangulation.html#a11f786cd9e9649ab0fe5f852d1c790ac',1,'CDT::Triangulation::insertEdges(const std::vector&lt; Edge &gt; &amp;edges)']]],
+  ['insertvertices',['insertVertices',['../classCDT_1_1Triangulation.html#ab6d92e2ee0c58eb0e02b135e18495cc8',1,'CDT::Triangulation::insertVertices(TVertexIter first, TVertexIter last, TGetVertexCoordX getX, TGetVertexCoordY getY)'],['../classCDT_1_1Triangulation.html#a057e1ddfc5304309d0cedcc498226be6',1,'CDT::Triangulation::insertVertices(const std::vector&lt; V2d&lt; T &gt; &gt; &amp;vertices)']]],
+  ['isincircumcircle',['isInCircumcircle',['../namespaceCDT.html#ac08e464ee04d71d5f438ec3baf1332b0',1,'CDT::isInCircumcircle(const V2d&lt; T &gt; &amp;p, const V2d&lt; T &gt; &amp;v1, const V2d&lt; T &gt; &amp;v2, const V2d&lt; T &gt; &amp;v3)'],['../namespaceCDT.html#aaec66697478c6fc41f7b8fed718fd6c7',1,'CDT::isInCircumcircle(const V2d&lt; T &gt; &amp;p, const V2d&lt; T &gt; &amp;v1, const V2d&lt; T &gt; &amp;v2, const V2d&lt; T &gt; &amp;v3)']]],
+  ['isonedge',['isOnEdge',['../namespaceCDT.html#a8ebd98ea276e82c059b20274193382b9',1,'CDT']]]
+];
